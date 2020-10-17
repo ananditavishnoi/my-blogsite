9f565df35df2d5df4fe4f97017eac74b9abace14
ananditavishnoi <anandita.vishnoi@gmail.com>
2020-10-17 13:51:47 +0530

    started blog and added GIFs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,8 @@ const plugins = require('./config/plugins/')
 
 module.exports = {
     // dest: 'gb-pages',//打包目录
-    base: '/Aanablog/',
-    title: "aana'sblogs",
+    base: '/vnote/',
+    title: "vnote",
     description: ' to feed my enthusiasm for writing ',
     head: [
         ['link', {
@@ -29,53 +29,33 @@ module.exports = {
         }]
     ],
 
-    //主题配置
     theme: 'reco',
     themeConfig: {
         nav,
         // sidebar: 'auto',
         sidebar,
         // logo: '/vuepress/head-fish.jpg', 
-        authorAvatar: '/vuepress/head-fish.jpg',
+        authorAvatar: '/vuepress/fish.jpg',
         search: true,
         searchMaxSuggestions: 10,
         sidebarDepth: 2,
-        blogConfig: {
-            category: {
-                location: 2, // position in nav bar menu, default 2
-                text: '分类' // default 'copywriting' category
-            },
-            tag: {
-                location: 3, // The position occupied in the navigation bar menu, default 3
-                text: '标签' // Default copy "label"
-            }
-        },
+        // blogConfig: {
+        //     category: {
+        //         location: 2, // position in nav bar menu, default 2
+        //         text: '分类' // default 'copywriting' category
+        //     },
+        //     tag: {
+        //         location: 3, // The position occupied in the navigation bar menu, default 3
+        //         text: '标签' // Default copy "label"
+        //     }
+        // },
         // Last update time
         lastUpdated: true,
         lastUpdated: 'last updated', // string | boolean
         // Author
         author: 'Anandita Vishnoi',
         // record number
-        record: '京ICP备17067634号-1',
-        // project start time
-        startYear: '2019',
-        //git地址
-        repo: 'zpj80231/znote',
-        // 如果你的文档不在仓库的根部
-        docsDir: 'docs',
-        // 可选，默认为 master
-        //docsBranch: 'vuepress',
-        editLinks: true,
-        editLinkText: '在 GitHub 上编辑此页！',
-        //评论
-        valineConfig: {
-            appId: 'xbT4pPXCWQju5T7GXC5fUFlF-gzGzoHsz',
-            appKey: 'bofA5chNQ60p37Ih9sMffSI0',
-            placeholder: '填写邮箱可以收到回复哦!',
-            notify: true, // 邮件提醒
-            verify: true, // 验证码
-            recordIP: true
-        },
+
     },
     plugins,
     markdown: {
